8a3e2a311709c7d9cbe38abad63e708fa2066e3d
taoky <taoky1999@gmail.com>
2018-10-14 20:37:39 +0800

    adjust struct & add easter egg

---
+++ src_and_exp/helper.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# This script helps you decode "her poem"
+
+from codecs import decode
+
+
+if __name__ == '__main__':
+    fin = open("poem.txt", "r")
+    fout = open("poem.out", "w")
+
+    for i in fin:
+        data = "begin 666 <data>\n" + i + " \nend\n"
+        decode_data = decode(data.encode("ascii"), "uu")
+        print(decode_data)
+        fout.write(decode_data.decode("ascii") + "\n")
+
+    fin.close()
+    fout.close()
